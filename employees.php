c036b3de119b6ed1ac26918090a0eef4c6eb91e3
unknown <tawakalshamss@gmail.com>
2020-04-19 08:11:53 -0700

    Nimetengeneza form katk page ya employee.php na kudisplay

--- employees.php
+++ employees.php
@@ -7,17 +7,109 @@
 <div class="content-wrapper">
     <!-- START PAGE CONTENT-->
     <div class="page-content fade-in-up">
-        <div class="row">
-            <div class="col-lg-12">
+          <div class="row">
+            <div class="col-sm-12">
+                <div class="ibox">
+                    <div class="ibox-body">
+                         <h3>Employees</h3><br>
+                        <div class="flexbox mb-4">
+                        <div class="col-sm-12">
+                        <div class="row">
+                        <div class="col-sm-4">
+                             <div class="form-group">
+                                 <label>Full Name:</label>
+                                 <input type="text" name="name" class="form-control">
+                             </div>
+                        </div>
+                        <div class="col-sm-4">
+                             <div class="form-group">
+                                 <label>Address:</label>
+                                 <input type="text" name="address"  class="form-control">
+                             </div>
+                        </div>
+                        <div class="col-sm-4">
+                             <div class="form-group">
+                                 <label>Gender:</label>
+                                 <select class="form-control" name="gender">
+                                     <option value="">Select Gender</option>
+                                     <option value="male">Male</option>
+                                     <option value="female">Female</option>
+                                 </select>
+                             </div>
+                        </div>
+                          <div class="col-sm-4">
+                             <div class="form-group">
+                                 <label>Phone Number:</label>
+                                 <input type="text" name="phone" class="form-control">
+                             </div>
+                        </div>
+                          <div class="col-sm-4">
+                             <div class="form-group">
+                                 <label>Employee Status:</label>
+                                 <select class="form-control" name="gender">
+                                     <option value="">Select Employee Status</option>
+                                     <option value="account">Account</option>
+                                     <option value="bank">Banking</option>
+                                     <option value="it">It</option>
+                                     <option value="pro">Procurement</option>
+                                 </select>
+                             </div>
+                        </div>
+                        </div>  
+                        </div> 
+
+                        </div>
+                  
+                    </div>
+                </div>
+            </div>
+
+            <div>
+            </div>
+        </div>
+          <div class="row">
+            <div class="col-sm-12">
                 <div class="ibox">
                     <div class="ibox-body">
                         <div class="flexbox mb-4">
-                            <div>
-                                <h3 class="m-0">Employees</h3>
-                            </div>
-                            <div class="d-inline-flex">
-                                
-                            </div>
+                           <table class="table table-striped table-border">
+                               <tr>
+                                   <thead style="background-color:black;color:#ffffff; ">
+                                       <td>#</td>
+                                       <td>Full Name</td>
+                                       <td>Address</td>
+                                       <td>Gender</td>
+                                       <td>Phone Number</td>
+                                       <td>Employee Status</td>
+                                   </thead>
+                               </tr>
+                               <tbody>
+                                   <tr>
+                                        <td>1</td>
+                                        <td>Tawakal Shams</td>
+                                        <td>Tomondo</td>
+                                        <td>Male</td>
+                                        <td>0674303210</td>
+                                        <td>It</td>
+                                   </tr>
+                                     <tr>
+                                        <td>2</td>
+                                        <td>Mpembainc</td>
+                                        <td>Kojan</td>
+                                        <td>Male</td>
+                                        <td>0998776</td>
+                                        <td>It</td>
+                                   </tr>
+                                      <tr>
+                                        <td>3</td>
+                                        <td>Yussuf Shams</td>
+                                        <td>Chwaka</td>
+                                        <td>Male</td>
+                                        <td>099338776</td>
+                                        <td>Banking</td>
+                                   </tr>
+                               </tbody>
+                           </table>
                         </div>
                     </div>
                 </div>
